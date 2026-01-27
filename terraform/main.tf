415a0d3ac3014215dce7f2be5dc07f1cfeb86fe0
anupsharma329 <anupsharma329@gmail.com>
2026-01-27 14:30:50 +0530

    add terraform EC2 infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,47 @@ terraform {
         region = "us-east-1"
     }
 
+    # Create SSH Key Pair
+    resource "aws_key_pair" "deployer" {
+        key_name   = "deployer-key"
+        public_key = file("~/.ssh/id_rsa.pub")  # Use your existing public key
+    }
+
+    # Security Group for EC2
+    resource "aws_security_group" "allow_ssh_http" {
+        vpc_id = aws_vpc.main.id
+        name_prefix = "allow_ssh_http"
+        description = "Allow SSH and HTTP traffic"
+
+        ingress {
+            from_port   = 22
+            to_port     = 22
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]  # Change this to your IP for security
+        }
+
+        ingress {
+            from_port   = 80
+            to_port     = 80
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+            from_port   = 3000
+            to_port     = 3000
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
+
     resource "aws_vpc" "main" {
         cidr_block = "10.0.0.0/16"
     }
@@ -22,9 +63,13 @@ terraform {
     }
 
     resource "aws_instance" "my_ec2" {
-        ami           = "ami-0b6c6ebed2801a5cb" # Ubuntu Linux 2 AMI
-        instance_type = "t2.micro"
-        subnet_id = aws_subnet.public.id
+        ami                    = "ami-0b6c6ebed2801a5cb" # Ubuntu Linux 2 AMI
+        instance_type          = "t2.micro"
+        subnet_id              = aws_subnet.public.id
+        vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+        key_name               = aws_key_pair.deployer.key_name
+        associate_public_ip_address = true
+
         user_data = <<-EOF
                     #!/bin/bash
                     yum update -y
@@ -35,4 +80,15 @@ terraform {
         tags = {
             Name = "DemoInstance-${terraform.workspace}"
         }
+    }
+
+    # Outputs
+    output "ec2_public_ip" {
+        description = "The public IP of the EC2 instance"
+        value       = aws_instance.my_ec2.public_ip
+    }
+
+    output "ec2_instance_id" {
+        description = "The ID of the EC2 instance"
+        value       = aws_instance.my_ec2.id
     }
